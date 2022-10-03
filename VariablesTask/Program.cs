c1d75da12753142d0894ac6b2c0ad6828cee8a04
Jon Joyce <531905@escg.ac.uk>
2022-10-03 15:07:26 +0100

    first commit

---
+++ VariablesTask/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace VariablesTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print Hello World!
+            Console.WriteLine("Hello World!");
+
+            //store and print a number
+            int myNumber = 1;
+            Console.WriteLine(myNumber);
+
+            //set and print a number
+            myNumber = 2;
+            Console.WriteLine(myNumber);
+
+            //store and print a word
+            string myWord = "apple";
+            Console.WriteLine(myWord);
+
+            //store and print an array of numbers
+            int[] myNumbers = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(myNumbers);
+        }
+    }
+}
